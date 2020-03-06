42083710da5de22adc5f3e74e2b2ab64fdadbfe7
Martín Matayoshi <kamikaizer@gmail.com>
2020-03-06 16:57:05 -0300

    clase 29

--- Clase29.js
+++ Clase29.js
@@ -1,14 +1,15 @@
 const API_URL = 'https://swapi.co/api/'
 const PEOPLE_URL ='people/:id'
-
-const charUrl = `${API_URL}${PEOPLE_URL.replace(':id', 4)}`
 const opts = { crossDomain: true }
 // con la primer constante se busca en la url la persona y con el replace se cambia el :id por el numero del personaje
 // crossDomain indica que se hara una peticion hacia otra pagina
 
+const onPeopleResponse = function (persona){
+  console.log(`Hola soy, ${persona.name}`)
+}
 
-// CALLBACK
-$.get(charUrl, opts, function (character) {
-  console.log(`Hola yo soy ${character.name}`)
-})
-// esto es el resultado de la consulta de las lineas 4, 5
+function obtenerPersonaje(id) {
+  const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+  $.get(url, opts, onPeopleResponse)
+}
+obtenerPersonaje(1)
